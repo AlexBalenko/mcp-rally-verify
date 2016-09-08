d5c98b7b6aa0122d15672647c2f8ccd89ae467a0
obutenko <obutenko@mirantis.com>
2016-09-08 16:52:42 +0300

    Fix clean script

--- clean.sh
+++ clean.sh
@@ -3,5 +3,5 @@ image_id=$(docker images | grep 0.6.0 | awk '{print $3}')
 docker_list=$(docker ps -a| grep $image_id |awk '{print$1}')
 docker rm -f $docker_list
 rm -rf /home/.rally
-rm /home/install_tempest.sh /home/openrc /home/.rally.sqlite /home/run_debug.sh
+rm /home/install_tempest.sh /home/openrc /home/.rally.sqlite /home/debug
 rm /home/public_haproxy.pem
