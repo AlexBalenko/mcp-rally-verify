a4f802b3026e16637a1cd3a1ba5eb4ec3d97a807
AlexBalenko <abalenko84@gmail.com>
2017-02-22 12:34:26 +0200

    Update prepare_env.sh

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -xe
 
 function prepare {
-    cp /root/openrc /home
+    cp /root/keystonerc /home
     cp debug /home
     cp install_tempest.sh /home
-    cp	ceph lvm skip_ceph.list skip_lvm.list /home
+#    cp	ceph lvm skip_ceph.list skip_lvm.list /home
 
-    V2_FIX=$(cat /home/openrc |grep v2.0| wc -l)
+    V2_FIX=$(cat /home/keystonerc |grep v2.0| wc -l)
     if [ ${V2_FIX} == '0' ]; then
         sed -i 's|:5000|:5000/v2.0|g' /home/openrc
     else
         echo "openrc file already fixed"
     fi
     
-    IS_TLS=$(source /root/openrc; openstack endpoint show identity 2>/dev/null | awk '/https/')
+    IS_TLS=$(source /root/keystonerc; openstack endpoint show identity 2>/dev/null | awk '/https/')
     if [ "${IS_TLS}" ]; then
         cp /var/lib/astute/haproxy/public_haproxy.pem /home 
         echo "export OS_CACERT='/home/rally/public_haproxy.pem'" >> /home/openrc
@@ -23,8 +23,8 @@ function prepare {
 function install_docker_and_run {
     apt-get install -y docker.io
     apt-get install -y cgroup-bin
-    docker pull rallyforge/rally:0.6.0
-    image_id=$(docker images | grep 0.6.0| awk '{print $3}')
+    docker pull rallyforge/rally:latest
+    image_id=$(docker images | grep latest| awk '{print $3}')
     docker run --net host -v /home/:/home/rally -tid -u root $image_id
     docker_id=$(docker ps | grep $image_id | awk '{print $1}'| head -1)
 }
