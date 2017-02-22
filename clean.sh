e2421eee1b06364e1535dab054221d22b45110f0
AlexBalenko <abalenko84@gmail.com>
2017-02-22 12:21:44 +0200

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
-image_id=$(docker images | grep 0.6.0 | awk '{print $3}')
+image_id=$(docker images | grep latest | awk '{print $3}')
 docker_list=$(docker ps -a| grep $image_id |awk '{print$1}')
 docker rm -f $docker_list
 rm -rf /home/.rally
-rm /home/install_tempest.sh /home/openrc /home/.rally.sqlite /home/debug
-rm /home/public_haproxy.pem
+rm /home/install_tempest.sh /home/keystone /home/.rally.sqlite /home/debug
+#rm /home/public_haproxy.pem
