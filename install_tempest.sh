d5dbcd7c15f40e2dea61a30615b33eb6bca39d7d
AlexBalenko <abalenko84@gmail.com>
2017-02-22 12:25:39 +0200

    Update install_tempest.sh
    update for the rally v. 8

--- install_tempest.sh
+++ install_tempest.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -xe
 sed -i 's|#swift_operator_role = Member|swift_operator_role = SwiftOperator|g' /etc/rally/rally.conf
-source /home/rally/openrc
+source /home/rally/keystonerc
 rally-manage db recreate
 rally deployment create --fromenv --name=tempest 
-rally verify install --add-options $storage_protocol
-rally verify genconfig
+#rally verify install --add-options $storage_protocol
+rally verify create-verifier --type tempest --name tempest-verifier
+#rally verify genconfig
+rally verify configure-verifier
